17d11aeb7988db0cf70ec8209024deedf6ac1efe
Magnus <magnusrk90@gmail.com>
2022-11-07 16:06:43 +0100

    Added Color enum

--- src/main/java/org/g16/MonopolyJR/PropertyField.java
+++ src/main/java/org/g16/MonopolyJR/PropertyField.java
@@ -3,8 +3,9 @@ package org.g16.MonopolyJR;
 public class PropertyField extends Field {
     protected int price;
     protected int rent;
-    
+    protected Color color;
     public PropertyField(){
 
     }
+
 }
