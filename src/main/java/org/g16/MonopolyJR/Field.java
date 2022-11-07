f37b9095dc27f300029c5807b5c1cf44ea0f29c1
Magnus <magnusrk90@gmail.com>
2022-11-07 16:00:31 +0100

    Added PropertyField

--- src/main/java/org/g16/MonopolyJR/Field.java
+++ src/main/java/org/g16/MonopolyJR/Field.java
@@ -3,8 +3,9 @@ package org.g16.MonopolyJR;
 public class Field {
 
     protected String name;
-    /*Switch case to choose which field is landed on.
-     It chooses based on the dice throw and outputs the corresponding field.*/
+    public Field(){
+
+    }
     public int getField(int dieCount) {
         return 0;
         }
---
+++ src/main/java/org/g16/MonopolyJR/PropertyField.java
@@ -0,0 +1,10 @@
+package org.g16.MonopolyJR;
+
+public class PropertyField extends Field {
+    protected int price;
+    protected int rent;
+    
+    public PropertyField(){
+
+    }
+}
