9e21357d08c59420d9837d1d34737cbe41f4dd55
MathiasW <s224293@student.dtu.dk>
2022-11-11 13:51:48 +0100

    Created methods in chancecard

--- src/main/java/org/g16/MonopolyJR/ChanceCard.java
+++ src/main/java/org/g16/MonopolyJR/ChanceCard.java
@@ -1,6 +1,26 @@
 package org.g16.MonopolyJR;
 
+import java.util.stream.IntStream;
+
 public class ChanceCard {
+
+    public int Birthday(Player[] players,int playerturn){
+        int addcash= 1;
+        String string= "It's your birthday, everyone gives you one dollar";
+        return addcash;
+    }
+
+    public int movefive(){
+        return 5;
+    }
+
+    public int Toomuchcandy(){
+        int addcash =-2;
+        String candy= "You ate too much candy, pay the bank 2 dollars";
+        return addcash;
+    }
 
 
 }
+
+
