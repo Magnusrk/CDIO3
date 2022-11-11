d0507faaffb7c502dc57c12b6525778f739d6a58
Magnus <magnusrk90@gmail.com>
2022-11-11 11:24:03 +0100

    started devloping playround

--- src/main/java/org/g16/MonopolyJR/GameController.java
+++ src/main/java/org/g16/MonopolyJR/GameController.java
@@ -2,30 +2,85 @@ package org.g16.MonopolyJR;
 
 public class GameController {
     private boolean winnerFound = false;
+    private Player player1 = new Player(Token.Cat);
+    private Player player2 = new Player(Token.Car);
+    private Player player3 = new Player(Token.Dog);
+    private Player player4 = new Player(Token.Ship);
+
     public GameController(){
 
     }
 
     public void play(){
         Language.SetLanguage("dansk");
-        playRound();
+        playRound(1);
     }
-    private void playRound(){
-        Player player1 = new Player(Token.Cat);
+
+    private void setup(){
+        player1.setAge(10);
+        player2.setAge(12);
+        player3.setAge(14);
+        player4.setAge(9);
+    }
+    private void playRound(int pt) {
+        Player currentPlayer = null;
+        switch (pt){
+            case 1:
+                currentPlayer = player1;
+                break;
+            case 2:
+                currentPlayer = player2;
+                break;
+            case 3:
+                currentPlayer = player3;
+                break;
+            case 4:
+                currentPlayer = player4;
+                break;
+        }
+        if (currentPlayer.getJailed()){
+            if (currentPlayer.getPlayerBalance() > 1){
+                currentPlayer.AddBalance(-1);
+            }
+        }
+
         int roll = Die.throwDie();
-        player1.setPlayerPosition(4);
-        System.out.println(player1.getPlayerPosition());
-        if (getField(player1.getPlayerPosition()) instanceof PropertyField property){
-            System.out.println(property.getPrice());
-            System.out.println(property.getColor());
-            System.out.println(Language.GetString("roll"));
-            System.out.println(property.getName());
-            property.setOwner(player1);
-            System.out.println(property.getOwner().getPlayerToken());
-        } else if (getField(player1.getPlayerPosition()) instanceof VisitorField visitor) {
-            System.out.println(visitor.getName());
-        } else if (getField(player1.getPlayerPosition()) instanceof  ChanceField chance) {
-            System.out.println(chance.getName());
+        currentPlayer.setPlayerPosition(1);
+        System.out.println(currentPlayer.getPlayerPosition());
+
+        if (currentPlayer.getPlayerPosition() < currentPlayer.getPrevPlayerPosition()){
+            currentPlayer.AddBalance(2);
+            System.out.println("Passed start");
+        }
+        if (getField(currentPlayer.getPlayerPosition()) instanceof PropertyField property) {
+            System.out.println(property);
+            if (property.getOwner() == null) {
+                currentPlayer.AddBalance(-1 * property.getPrice());
+                System.out.println("You pay " + property.getPrice());
+                getField(1).getOwner(currentPlayer);
+                System.out.println(property.getOwner());
+            }else {
+                currentPlayer.AddBalance(-1*property.getPrice());
+                property.getOwner().AddBalance(property.getPrice());
+                System.out.println("You pay " + property.getPrice() + "to" + property.getOwner());
+            }
+        } else if (getField(currentPlayer.getPlayerPosition()) instanceof VisitorField visitor) {
+            System.out.println("visit");
+
+        } else if (getField(currentPlayer.getPlayerPosition()) instanceof ChanceField chance) {
+            System.out.println("Chance");
+
+        } else if (getField(currentPlayer.getPlayerPosition()) instanceof  GoToJailField goToJail){
+            currentPlayer.setJailed();
+            System.out.println("You're jailed");
+        }
+        if (!winnerFound){
+            if (pt == 4){
+                pt = 1;
+            }else {
+                pt++;
+            }
+            playRound(pt);
         }
 
     }
--- src/main/java/org/g16/MonopolyJR/Player.java
+++ src/main/java/org/g16/MonopolyJR/Player.java
@@ -3,7 +3,10 @@ package org.g16.MonopolyJR;
 public class Player {
     MoneyBalance balance = new MoneyBalance();
     private int playerPosition = 0;
+    private int prevPlayerPosition = 0;
     Token playerToken;
+    private int age;
+    private boolean jailed = false;
 
     public Player(Token playerToken) {
         this.playerToken = playerToken;
@@ -15,8 +18,20 @@ public class Player {
     public int getPlayerPosition(){
         return playerPosition;
     }
+    public int getPrevPlayerPosition(){
+        return prevPlayerPosition;
+    }
     public Token getPlayerToken(){
         return playerToken;
+    }
+    public int getAge(){
+        return age;
+    }
+    public boolean getJailed(){
+        return jailed;
+    }
+    public void setAge(int age){
+        this.age = age;
     }
 
     public void AddBalance(int add){
@@ -24,7 +39,11 @@ public class Player {
     }
 
     public void setPlayerPosition(int position){
+        prevPlayerPosition = playerPosition;
         playerPosition = position;
+    }
+    public void setJailed(){
+        jailed = true;
     }
     public void reset(){
         balance.reset();
