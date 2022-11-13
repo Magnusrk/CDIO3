6d00028a0e1cd4953c4f194593041cc02077d701
MathiasW <mathiasw01@hotmail.com>
2022-11-13 15:27:16 +0100

    Started filling in logic behind the Chancecards

--- src/main/java/org/g16/MonopolyJR/ChanceField.java
+++ src/main/java/org/g16/MonopolyJR/ChanceField.java
@@ -1,5 +1,7 @@
 package org.g16.MonopolyJR;
 
+import java.util.Arrays;
+
 public class ChanceField extends Field {
 
     public ChanceField(String name) {
@@ -7,10 +9,17 @@ public class ChanceField extends Field {
     }
 
     public int[] drawChancecard() {
-        ChancecardController chancecardController = new ChancecardController();
-        chancecardController.getNumchance();
-        chancecardController.leftshiftarray();
-        chancecardController.setNumchance(chancecardController.leftshiftarray());
-        return chancecardController.getNumchance();
+        Chancecard chancecard = new Chancecard();
+        chancecard.getNumchance();
+        chancecard.setNumchance(chancecard.leftshiftarray());
+        return chancecard.getNumchance();
+    }
+
+    public static void main(String[] args) {
+        Chancecard chancecard = new Chancecard();
+        chancecard.getNumchance();
+        chancecard.setNumchance(chancecard.leftshiftarray());
+        System.out.println(Arrays.toString(chancecard.getNumchance()));
+
     }
 }
