5f1d8105c848d7b0a8efd5def9437e41fb210e91
MathiasW <s224293@student.dtu.dk>
2022-11-18 10:38:50 +0100

    ?

--- src/main/java/org/g16/MonopolyJR/GameController.java
+++ src/main/java/org/g16/MonopolyJR/GameController.java
@@ -217,6 +217,7 @@ public class GameController {
 
         switch (chancecard.getNumchance()[0]) {
             case 1 -> {
+
                 for (Player player : players) {
                     if (player.playerToken == Token.Ufo) {
                         player.setTokenChancecard(true);
